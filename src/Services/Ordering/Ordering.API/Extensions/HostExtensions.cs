9f967907563c960dce2eaa6e3c7687e6244287f6
Israel Gelover <israel.gelover@agilethought.com>
2022-02-07 22:19:00 -0600

    Update for backup

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+
+
+            return host;
+        }
+    }
+}
