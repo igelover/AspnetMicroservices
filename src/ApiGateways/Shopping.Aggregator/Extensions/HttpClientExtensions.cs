0a57157d00e200139e0dd77ad4676d9379e5b80a
Israel Gelover <israel.gelover@agilethought.com>
2022-05-06 17:30:01 -0500

    Add Shopping.Aggregator project and container

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"An error ocurred while calling the API: {response.ReasonPhrase}");
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
