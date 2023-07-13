f1a0e598e48a8d383a853943a427cd9f7dcc9bbb
Israel Gelover <v-igelover@youngliving.com>
2023-07-13 14:18:47 -0600

    Pump .Net versionto 6 and update packages

--- src/Services/Ordering/Ordering.Application/Behaviors/UnhandledExceptionBehavior.cs
+++ src/Services/Ordering/Ordering.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -15,7 +15,7 @@ namespace Ordering.Application.Behaviors
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             try
             {
