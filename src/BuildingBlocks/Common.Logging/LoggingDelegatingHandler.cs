be44ad895a876eae94502ee3653c21118d9d6f68
Israel Gelover <israel@gelover.com>
2023-11-24 20:30:01 -0600

    Add LoggingDelegatingHandler for reusable  HTTP requests logging.

---
+++ src/BuildingBlocks/Common.Logging/LoggingDelegatingHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Logging;
+
+namespace Common.Logging
+{
+    public class LoggingDelegatingHandler : DelegatingHandler
+    {
+        private readonly ILogger<LoggingDelegatingHandler> _logger;
+
+        public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Received a sucess response from {Url}", response?.RequestMessage?.RequestUri);
+                }
+                else
+                {
+                    _logger.LogWarning("Received a non-sucess statusCode {StatusCode} from {Url}", (int)response.StatusCode, response?.RequestMessage?.RequestUri);
+                }
+
+                return response;
+            }
+            catch (HttpRequestException ex)
+                when (ex.InnerException is SocketException se && se.SocketErrorCode == SocketError.ConnectionRefused)
+            {
+                var hostWithPort = request.RequestUri.IsDefaultPort ? request.RequestUri.DnsSafeHost : $"{request.RequestUri.DnsSafeHost}:{request.RequestUri.Port}";
+
+                _logger.LogCritical(ex, "Unable to connect to {host}, please check the configuration to ensure the correct URL for the service has been configured.", hostWithPort);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.BadGateway)
+            {
+                RequestMessage = request
+            };
+        }
+    }
+}
