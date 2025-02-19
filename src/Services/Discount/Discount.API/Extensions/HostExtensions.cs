95c1c0421f5c56ea2d43e6ab7a7cfe7ac9b9eb7b
Israel Gelover <israel@gelover.com>
2025-02-18 22:22:13 -0600

    Add Polly to Postgres migrations on Discount microservices for resiliency

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -3,71 +3,86 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
-using System.Threading;
+using Polly;
+using System;
 
 namespace Discount.API.Extensions
 {
     public static class HostExtensions
     {
-        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        public static IHost MigrateDatabase<TContext>(this IHost host)
         {
-            int retryForAvailability = retry.Value;
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 var configuration = services.GetRequiredService<IConfiguration>();
                 var logger = services.GetRequiredService<ILogger<TContext>>();
+                var maxRetries = 5;
 
                 try
                 {
-                    logger.LogInformation("Migrating postgresql database.");
+                    logger.LogInformation("Migrating the PostgreSQL database.");
 
-                    using var connection = new NpgsqlConnection
-                        (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                    connection.Open();
+                    var retry = Policy.Handle<NpgsqlException>()
+                        .WaitAndRetry(
+                            retryCount: maxRetries,
+                            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                            onRetry: (exception, timeSpan, retry, context) =>
+                            {
+                                logger.LogError(exception,
+                                    "[{prefix}] {ExceptionType}: {Message} on attempt {retry} of {retries}",
+                                    typeof(TContext).Name,
+                                    exception.GetType().Name,
+                                    exception.Message,
+                                    retry,
+                                    maxRetries);
+                            });
 
-                    using var command = new NpgsqlCommand
-                    {
-                        Connection = connection
-                    };
+                    retry.Execute(() => ExecuteMigrations(configuration));
 
-                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
-                    command.ExecuteNonQuery();
+                    logger.LogInformation("Migrated the PostgreSQL database.");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the PostgreSQL database.");
+                }
+            }
+            return host;
+        }
 
-                    command.CommandText = @"CREATE TABLE Coupon(
+        private static void ExecuteMigrations(IConfiguration configuration)
+        {
+            using var connection = new NpgsqlConnection(
+                configuration.GetValue<string>("DatabaseSettings:ConnectionString")
+            );
+            connection.Open();
+            
+            using var command = new NpgsqlCommand
+            {
+                Connection = connection
+            };
+            
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(
                                                 Id SERIAL PRIMARY KEY,
                                                 ProductName VARCHAR(24) NOT NULL,
                                                 Description TEXT,
                                                 Amount INT)";
-                    command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
 
-                    command.CommandText = @"INSERT INTO Coupon
+            command.CommandText = @"INSERT INTO Coupon
                                                 (ProductName, Description, Amount)
                                             VALUES
                                                 ('IPhone X', 'IPhone Discount', 150);";
-                    command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
 
-                    command.CommandText = @"INSERT INTO Coupon
+            command.CommandText = @"INSERT INTO Coupon
                                                 (ProductName, Description, Amount) 
                                             VALUES
                                                 ('Samsung 10', 'Samsung Discount', 100);";
-                    command.ExecuteNonQuery();
-
-                    logger.LogInformation("Migrated postresql database.");
-                }
-                catch (NpgsqlException ex)
-                {
-                    logger.LogError(ex, "An error occurred while migrating the postresql database");
-
-                    if (retryForAvailability < 50)
-                    {
-                        retryForAvailability++;
-                        Thread.Sleep(2000);
-                        MigrateDatabase<TContext>(host, retryForAvailability);
-                    }
-                }
-            }
-            return host;
+            command.ExecuteNonQuery();
         }
     }
 }
